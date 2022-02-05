296e0ebea44d04e940e7843fe63582413573f616
FilipD <filip.d@sourcico.com>
2022-02-06 00:49:40 +0100

    added redux and alert component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Register from './components/auth/Register';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
 
-function App() {
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Landing />} />
+          </Routes>
+          <Routes>
+            <Route exact path='/register' element={<Register />} />
+            <Route exact path='/login' element={<Login />} />
+          </Routes>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
